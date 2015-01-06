9108c93a6832f07ebd83919743644b22473553f4
Ahmad Soliman <ahmadadel92@gmail.com>
2015-01-06 18:30:15 +0200

    list of hot errors in any porject

--- airbrake_monitor.rb
+++ airbrake_monitor.rb
@@ -1,7 +1,18 @@
 require 'airbrake_tools'
+require 'ostruct'
 
 $config = YAML.load_file(File.join(Dir.pwd, 'config.yml'))
 AirbrakeTools.init($config['auth']['account'], $config['auth']['auth_token'])
+
+$projects = []
+$projects_hash = {}
+$config['project_names'].zip($config['project_ids']).each do |name, id|
+  project = OpenStruct.new
+  project.name = name
+  project.id = id
+  $projects.push(project)
+  $projects_hash[id] = name
+end
 
 # set utf-8 for outgoing
 before do
@@ -14,5 +25,12 @@ get '/' do
 end
 
 get '/hot' do
-  AirbrakeTools.hot_errors('107364').inspect
+  @title = 'Hot Errors'
+  @projectid = params[:projectid]
+  @project_name = $projects_hash[@projectid]
+  if @projectid.nil?
+      return haml :projects
+  end
+  @errors = AirbrakeTools.hot_errors(@projectid)
+  haml :list
 end
